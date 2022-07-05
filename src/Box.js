9f30afff9edf10e95edd74c10e9753529faf9fcd
Ivy Nguyen <animeweirdo24@gmail.com>
2022-07-05 14:11:19 -0600

    Write box test and write implementation code for it

--- src/Box.js
+++ src/Box.js
@@ -1,4 +1,25 @@
+class Box {
+    constructor(height, width) {
+        this.height = height || 100
+        this.width = width || 100
+    }
+    calculateArea() {
+        let area = this.height * this.width
+        return area
+    }
+    increaseWidth(addition) {
+        let newWidth = this.width + addition
+        return newWidth
+    }
+    increaseHeight(addition) {
+        let newHeight = this.height + addition
+        return newHeight
+    }
+    increment(amount, value) {
+        let newValue = this[value] + amount
+        return newValue
+    }
+}
 
 
-
-// module.exports = Box;
+ module.exports = Box;
--- test/Box-test.js
+++ test/Box-test.js
@@ -1,3 +1,61 @@
-// const chai = require('chai');
-// const expect = chai.expect;
-// const Box = require('../src/Box');
+ const chai = require('chai');
+ const expect = chai.expect;
+ const Box = require('../src/Box');
+
+ describe('box', () => {
+
+    it('should return true', () => {
+        expect(true).to.equal(true)
+    })
+
+    it('should be a function', () => {
+        const box = new Box()
+
+        expect(Box).to.be.a('function')
+    })
+
+    it('should be an instance of Box', () => {
+        const box = new Box()
+
+        expect(box).to.be.an.instanceOf(Box)
+    })
+
+    it('should have a default height and width of 100', () => {
+        const box = new Box()
+
+        expect(box.height).to.equal(100)
+        expect(box.width).to.equal(100)
+    })
+
+    it('should be able to pass in a height and width', () => {
+        const box = new Box(50, 50)
+
+        expect(box.height).to.equal(50)
+        expect(box.width).to.equal(50)
+    })
+
+    it('should be able to calculate the area', () => {
+        const box = new Box()
+
+        expect(box.calculateArea()).to.equal(10000)
+    })
+
+    it('should be able to increase width by a provided value', () => {
+        const box = new Box()
+
+        expect(box.increaseWidth(10)).to.equal(110)
+    })
+
+    it('should be able to increase height by a provided value', () => {
+        const box = new Box()
+
+        expect(box.increaseHeight(10)).to.equal(110)
+    })
+
+    it('should be able to increment measurements', () => {
+        const box = new Box(60, 50)
+
+        expect(box.increment(10, 'height')).to.equal(70)
+        expect(box.increment(10, 'width')).to.equal(60)
+    })
+ })
